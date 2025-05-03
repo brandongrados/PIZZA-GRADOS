311e71c43783e420832a8b10d1235596f9465188
brandongrados <bgradosalvarez@gmail.com>
2025-05-03 16:09:23 -0500

    cambios en EL BACK 3 ERROR AL CREAR PRODUCTO

--- tienda/src/router/index.js
+++ tienda/src/router/index.js
@@ -10,16 +10,51 @@ const routes = [
     name: 'home',
     component: HomeView
   },
-  {path: '/login',name: 'login',component: ()=> import('@/views/LoginApp.vue')},
-  {path: '/shop',name: 'shop',component: ()=> import('@/views/productos/IndexProductoApp.vue')},
-  {path: '/cart',name: 'cart',component: ()=> import('@/views/CarritoView.vue')},
-  {path: '/producto/:slug',name: 'show-producto',component: ()=> import('@/views/productos/ShowProductoApp.vue')},
-  
-  {path: '/cuenta/direcciones',name: 'direcciones',component: ()=> import('@/views/cuenta/DireccionesApp.vue')},
-  {path: '/checkout',name: 'checkout',component: ()=> import('@/views/CheckoutView.vue')},
-  {path: '/verificacion/:estado/:direccion?',name: 'verificacion',component: ()=> import('@/views/VerificacionView.vue')},
-  {path: '/cuenta/venta/:id',name: 'venta',component: ()=> import('@/views/cuenta/ventas/VentaDetalleApp.vue')},
-  {path: '/cuenta/venta',name: 'venta-index',component: ()=> import('@/views/cuenta/ventas/VentaIndexApp.vue')},
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/LoginApp.vue')
+  },
+  {
+    path: '/shop',
+    name: 'shop',
+    component: () => import('../views/productos/IndexProductoApp.vue')
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: () => import('../views/CarritoView.vue')
+  },
+  {
+    path: '/producto/:slug',
+    name: 'show-producto',
+    component: () => import('../views/productos/ShowProductoApp.vue')
+  },
+  {
+    path: '/cuenta/direcciones',
+    name: 'direcciones',
+    component: () => import('../views/cuenta/DireccionesApp.vue')
+  },
+  {
+    path: '/checkout',
+    name: 'checkout',
+    component: () => import('../views/CheckoutView.vue')
+  },
+  {
+    path: '/verificacion/:estado/:direccion?',
+    name: 'verificacion',
+    component: () => import('../views/VerificacionView.vue')
+  },
+  {
+    path: '/cuenta/venta/:id',
+    name: 'venta',
+    component: () => import('../views/cuenta/ventas/VentaDetalleApp.vue')
+  },
+  {
+    path: '/cuenta/venta',
+    name: 'venta-index',
+    component: () => import('../views/cuenta/ventas/VentaIndexApp.vue')
+  }
 ]
 
 const router = new VueRouter({
